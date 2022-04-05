13c7145b2906b67fd4dc8e9ece8ff93363df1c7a
Marco Aurélio <malf88@gmail.com>
2022-04-04 21:37:53 -0300

    Novo Layout com material design.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
-import 'react-app-polyfill/stable'
-import 'core-js'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import * as serviceWorker from './serviceWorker'
-import { Provider } from 'react-redux'
-import store from './store'
+// scroll bar
+import 'simplebar/src/simplebar.css';
+
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+
+//
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
+
+// ----------------------------------------------------------------------
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root'),
-)
+  <HelmetProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </HelmetProvider>,
+  document.getElementById('root')
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
+// If you want to enable client cache, register instead.
+serviceWorker.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,41 @@
-import React from 'react'
+import { Navigate, useRoutes } from 'react-router-dom'
+// layouts
+import DashboardLayout from './layouts/dashboard';
+import LogoOnlyLayout from './layouts/LogoOnlyLayout';
+//
+import Login from './pages/Login'
+import Register from './pages/Register'
+import DashboardApp from './pages/DashboardApp'
+import Products from './pages/Products'
+import Blog from './pages/Blog'
+import User from './pages/User'
+import NotFound from './pages/Page404'
 
-//System Routes
-const Accounts = React.lazy(() => import('./views/accounts/accounts/Accounts'))
-const Categories = React.lazy(() => import('./views/accounts/categories/Categories'))
-const CategoryInsert = React.lazy(() => import('./views/accounts/categories/CategoryInsert'))
-const CategoryEdit = React.lazy(() => import('./views/accounts/categories/CategoryEdit'))
+// ----------------------------------------------------------------------
 
-const routes = [
-  //System Routes
-  { path: '/accounts', name: 'Accounts', component: Accounts },
-  { path: '/categories/edit', name: 'Edit category', component: CategoryEdit },
-  { path: '/categories/insert', name: 'Insert category', component: CategoryInsert },
-  { path: '/categories', name: 'Categories', component: Categories },
-
-  //End System Routes
-]
-
-export default routes
+export default function Router() {
+  return useRoutes([
+    {
+      path: '/dashboard',
+      element: <DashboardLayout />,
+      children: [
+        { path: 'app', element: <DashboardApp /> },
+        { path: 'user', element: <User /> },
+        { path: 'products', element: <Products /> },
+        { path: 'blog', element: <Blog /> }
+      ]
+    },
+    {
+      path: '/',
+      element: <LogoOnlyLayout />,
+      children: [
+        { path: '/', element: <Navigate to="/dashboard/app" /> },
+        { path: 'login', element: <Login /> },
+        { path: 'register', element: <Register /> },
+        { path: '404', element: <NotFound /> },
+        { path: '*', element: <Navigate to="/404" /> }
+      ]
+    },
+    { path: '*', element: <Navigate to="/404" replace /> }
+  ]);
+}
