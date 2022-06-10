11c0960dd57da78d187a6ef8247c4bad0748eb6b
Marco Aurélio <malf88@gmail.com>
2022-06-09 22:42:13 -0300

    Adicionado endpoint para buscar versão da API.

--- src/componentes/footer/Footer.js
+++ src/componentes/footer/Footer.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import styled from 'styled-components'
+import { getServiceWithoutToken } from '../../library/Service'
 
-const version = require('../../../package.json').version
+const versionApp = require('../../../package.json').version
 const FooterComponente = styled.div`
   height: 30px;
   text-align: center;
   font-family: 'Roboto', sans-serif;
   margin-top: 20px;
 `
-const Footer = () => (
-  <FooterComponente>
-    <span>App-Version: {version}</span> | <span>Api-Version: xxx</span>
-  </FooterComponente>
-)
+
+const Footer = () => {
+  const [versionApi, setVersionApi] = React.useState('')
+  React.useEffect(() => {
+    async function getVersionApp() {
+      await getServiceWithoutToken()
+        .get('/version')
+        .then((response) => {
+          setVersionApi(response.data)
+        })
+    }
+    getVersionApp()
+  }, [])
+  return (
+    <FooterComponente>
+      <span>App-Version: {versionApp}</span> | <span>Api-Version: {versionApi}</span>
+    </FooterComponente>
+  )
+}
 
 export default Footer
