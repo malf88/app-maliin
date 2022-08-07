56a8f412eeb22c4de5084c76a25f4d7319781c89
Marco Aurélio <malf88@gmail.com>
2022-08-06 21:43:28 -0300

    Criado gráfico de despesas e receitas na página principal.

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Container from '@mui/material/Container'
+import { Divider, Grid, Paper, Stack } from '@mui/material'
+import { styled } from '@mui/material/styles'
+import ReceitasDespesas from '../../componentes/Charts/ReceitasDespesas'
+import { listAccounts } from '../../componentes/Project/AccountActions'
+import { v4 } from 'uuid'
+import Box from '@mui/material/Box'
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}))
 
 const Home = () => {
+  const [accounts, setAccounts] = useState([])
+  useEffect(() => {
+    async function getAccounts() {
+      setAccounts(await listAccounts())
+    }
+    getAccounts()
+  }, [])
   return (
-    <div>
-      <h1>Página Inicial</h1>
-    </div>
+    <Container sx={{ marginLeft: 1 }}>
+      <h1>Dashboard</h1>
+      <Grid container spacing={2}>
+        {accounts.map((account) => (
+          <Grid item md={4} xs={11} key={v4()}>
+            <Item>
+              <ReceitasDespesas account={account} />
+            </Item>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   )
 }
 
